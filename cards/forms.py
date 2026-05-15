586fd9bc5f2f5d2ec7ac841cb4cda8fe950ef0a8
Zikrilloh <asralkhanov07@gmail.com>
2026-05-16 08:23:42 +0900

    Design changed from entirely css to css + bootstrap combination

--- cards/forms.py
+++ cards/forms.py
@@ -4,18 +4,9 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(
-        required=True,
-        widget=forms.EmailInput(attrs={
-            'class': 'auth-input',
-            'placeholder': 'name@example.com',
-            'autocomplete': 'email',
-        })
-    )
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('username','first_name', 'last_name', 'password1', 'password2')
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'auth-input',
